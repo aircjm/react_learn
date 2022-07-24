08737b88f942851482f437692af137ca563ecc61
aircjm <aircjm@gmail.com>
2022-07-24 23:40:51 +0800

    Mapping array of arrays

---
+++ resemi-admin/src/pages/map/arrayarray.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+// 通过map Mapping array of arrays
+export default function arraymap() {
+    const numbers = [
+      ["dev", 1],
+      ["test", 2],
+      ["front", 3],
+      ["back",  4]
+    ];
+
+    return (
+        <div className='container'>
+        <div>
+          <h1>Numbers List</h1>
+          <ul>
+          <NumberFor numbers={numbers}/>
+          </ul>
+        </div>
+      </div>
+    )
+}
+
+
+function NumberFor({numbers})  {
+    return (
+        numbers.map((number) => {return <li>{number[0]} +  {number[1]} </li>})
+    )
+}
